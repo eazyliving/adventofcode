8a6dce85297fe342e41b1c02da757d6cfe29c1dd
Christian Bednarek <eazy@eazy-living.de>
2021-11-30 15:40:42 +0100

    update

--- 2020/10_b.tcl
+++ 2020/10_b.tcl
@@ -118,39 +118,7 @@ set input {16
 6
 12
 4}
-
-set input2 {28
-33
-18
-42
-31
-14
-46
-20
-48
-47
-24
-23
-49
-45
-19
-38
-39
-11
-1
-32
-25
-35
-8
-17
-7
-9
-4
-2
-34
-10
-3}
-
+set input {1 4 5 6 7}
 set max [lindex [lsort -integer $input] end]
 incr max 3
 
