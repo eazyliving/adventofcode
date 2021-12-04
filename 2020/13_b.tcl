6109ce0ea9b9d9e8467a2428883161b5f95e6ad7
Christian Bednarek <eazy@eazy-living.de>
2021-12-04 12:06:15 +0100

    Update 13_b.tcl

--- 2020/13_b.tcl
+++ 2020/13_b.tcl
@@ -3,18 +3,43 @@ set input {
 19,x,x,x,x,x,x,x,x,41,x,x,x,37,x,x,x,x,x,821,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,463,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23
 }
 
-
 lassign $input ts busses
-set lines [regexp -inline -all {[0-9]+} $busses]
-set waits ""
 
-foreach line $lines {
+set idx 0
+set line {}
 
-	set div [expr $ts / $line.0]
-	lappend waits [list [expr round(ceil($div)*$line) - $ts] $line]
+foreach in [split [lindex $input 1] ,] {
+	if {$in!="x"} {
+		lappend lines [list $in $idx]
+	}
+	incr idx
 }
+set lines [join $lines]
+set w [llength [split [lindex $input 1] ,]]
+incr w -1
 
-set waits [lsort -integer -index 0 $waits]
 
-puts [expr [lindex $waits 0 0]*[lindex $waits 0 1]]
+set nums [regexp -inline -all {[0-9]+} $busses]
 
+set x [expr [join $nums *]]
+puts "starting at $x"
+while {1} {
+	
+	set f 0
+	if {[expr $x % 1000000]==0} {puts $x}
+	 foreach {line off} $lines {
+ 		if {[expr ($x+$off) % $line]!=0} {
+ 			set f 1;break
+ 		}
+	}
+	incr x
+}
+puts "r $x"
+
+exit
+
+	puts "x $w [expr ($x+$w)%23]"
+	if {[expr $x%19]==0 && [expr ($x+$w)%23]==0} {set f 1}
+	puts "f $f"
+	if {$f==1} {break}
+	incr x
