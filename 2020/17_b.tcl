605df5ee847a77a59a9a8bfa192931a9b0735093
Christian Bednarek <eazy@eazy-living.de>
2021-12-06 19:31:15 +0100

    finished 17_b

--- 2020/17_b.tcl
+++ 2020/17_b.tcl
@@ -9,7 +9,7 @@ set input {
 #.....##
 }
 
-proc actives {x y z} {
+proc actives {x y z w} {
 	
 	global matrix
 	
@@ -17,15 +17,17 @@ proc actives {x y z} {
 	for {set z1 [expr $z-1]} {$z1<=$z+1} {incr z1} {
 		for {set x1 [expr $x-1]} {$x1<=$x+1} {incr x1} {
 			for {set y1 [expr $y-1]} {$y1<=$y+1} {incr y1} {
+				for {set w1 [expr $w-1]} {$w1<=$w+1} {incr w1} {
 				#puts "$x1 $y1 $z1 "
 
-				if {$x1==$x&&$y1==$y&&$z1==$z} {continue}
+					if {$x1==$x&&$y1==$y&&$z1==$z&&$w1==$w} {continue}
 
-				if {![info exists matrix($x1,$y1,$z1)]} {
-					set matrix($x1,$y1,$z1) "."
-				} else {
-					if {$matrix($x1,$y1,$z1)=="#"} {
-						incr active 
+					if {![info exists matrix($x1,$y1,$z1,$w1)]} {
+						set matrix($x1,$y1,$z1,$w1) "."
+					} else {
+						if {$matrix($x1,$y1,$z1,$w1)=="#"} {
+							incr active 
+						}
 					}
 				}
 			}
@@ -39,11 +41,11 @@ set rounds 6
 
 set y 0
 set z 0
-
+set w 0
 foreach line $input {
 	set x 0
 	foreach status [split $line {}] {
-		set matrix($x,$y,$z) $status
+		set matrix($x,$y,$z,$w) $status
 		incr x		
 	}
 	incr y
@@ -51,25 +53,24 @@ foreach line $input {
 }
 
 foreach {key val} [array get matrix] {
-	lassign [split $key ,] x y z
-	set active [actives $x $y $z]
+	lassign [split $key ,] x y z w
+	set active [actives $x $y $z $w]
 }
 
 
 for {set round 0} {$round<$rounds} {incr round} {
-
 	array set buf [array get matrix]
 	foreach {key val} [array get matrix] {
-		lassign [split $key ,] x y z
+		lassign [split $key ,] x y z w
 
-		set active [actives $x $y $z]
+		set active [actives $x $y $z $w]
 		if {$val=="#"} {
 			if {$active!=2&&$active!=3} {
-				set buf($x,$y,$z) "."
+				set buf($x,$y,$z,$w) "."
 			}
 		} else {
 			if {$active==3} {
-				set buf($x,$y,$z) "#"
+				set buf($x,$y,$z,$w) "#"
 			}
 		}
 			
